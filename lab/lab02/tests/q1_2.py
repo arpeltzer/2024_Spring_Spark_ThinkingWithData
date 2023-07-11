cfaac77519d0e0ca6ab83b0196b7031c0d3291fa
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2023-07-10 23:26:07 -0700

    fix grader

--- lab/lab02/tests/q1_2.py
+++ lab/lab02/tests/q1_2.py
@@ -1,1 +1,3 @@
-test = {'name': 'q1_2', 'points': 0, 'suites': [{'cases': [{'code': '>>> avg_num_students == 354.25\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q1_2',
+    'points': 0,
+    'suites': [{'cases': [{'code': '>>> avg_num_students == 354.25\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- lab/lab02/tests/q2_1.py
+++ lab/lab02/tests/q2_1.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q2_1',
     'points': 0,
-    'suites': [   {   'cases': [{'code': '>>> num_rows_brfss == 100000\n', 'hidden': False, 'locked': False}, {'code': '>>> num_columns_brfss == 23\n', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> num_rows_brfss == 100000\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> num_columns_brfss == 23\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
