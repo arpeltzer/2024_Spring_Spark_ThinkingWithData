f5202b18dcc2ca5ca1d5c5eef963c8ead7201872
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2023-07-10 23:07:43 -0700

    lab02

---
+++ lab/lab02/tests/q1_2.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_2', 'points': 0, 'suites': [{'cases': [{'code': '>>> avg_num_students == 354.25\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
